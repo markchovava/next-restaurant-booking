89ec7e418fe223ed2b660b1215d70a115daa8156
unknown <markchovava@gmail.com>
2025-11-13 15:13:07 +0200

    Nav

--- src/_data/sample/NavData.tsx
+++ src/_data/sample/NavData.tsx
@@ -58,6 +58,6 @@ export const BottomNavData = [
 
 export const HelpNavData = [
   {id: 1, name: "Cancellation Policy", href: "/cancel-policy"},
-  {id: 1, name: "Reservation Policy", href: "/cancel-policy"},
+  {id: 1, name: "Reservation Policy", href: "/reservation-policy"},
   {id: 1, name: "Contact Us", href: "/contact"},
 ]
