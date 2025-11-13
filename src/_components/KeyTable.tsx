29fbf64fe0062eef8c59bc005d7d6dd1ea44ff7d
unknown <markchovava@gmail.com>
2025-11-13 16:45:46 +0200

    Q

--- src/_components/KeyTable.tsx
+++ src/_components/KeyTable.tsx
@@ -1,8 +1,13 @@
 "use client"
 
 import { KeyData } from "@/_data/sample/KeyData"
+import { useState } from "react"
+import { IoMdInformationCircleOutline } from "react-icons/io"
 
 export default function KeyTable() {
+  const [toggle, setToggle] = useState(false)
+
+
   return (
     <div className="absolute z-50 top-4 left-4 md:left-4 max-md:top-2 w-[60%] md:w-auto md:max-w-md">
         <div className="bg-white text-gray-800 border border-gray-300 rounded-lg shadow-lg p-3 md:p-4">
@@ -15,10 +20,25 @@ export default function KeyTable() {
                 </p>
               ))}
             </section>
-            <p className="text-xs md:text-sm mt-1 italic leading-tight">
+           {/*  <p className="text-xs md:text-sm mt-1 italic leading-tight">
               Click the <b>+</b> button to zoom in, <b>-</b> button to zoom out, the circular button to refresh. 
               Swipe across the floor and select the available seat marked in green.
-              
+            </p> */}
+        </div>
+
+        <div className="relative mt-4">
+            <button 
+                type='button' 
+                onClick={() => setToggle(!toggle)}>
+                <IoMdInformationCircleOutline
+                    className='text-slate-700 text-xl hover:text-black cursor-pointer' />
+            </button>
+            <p className={`${toggle 
+                ? 'opacity-100 translate-y-1 visible' // When visible
+                : 'opacity-0 -translate-y-0.5 invisible pointer-events-none'}
+                absolute text-sm font-light bg-white left-0 px-4 py-3 w-80 rounded-lg drop-shadow-lg`}>
+                Click the <b>+</b> button to zoom in, <b>-</b> button to zoom out, the circular button to refresh. 
+                Swipe across the floor and select the available seat marked in green.
             </p>
         </div>
     </div>
